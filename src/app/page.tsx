31adbc01e3f48e6396c9862dac0e6719421eff23
phalla.doll <phalla.doll@okoone.com>
2025-04-30 10:39:37 +0700

    Add framer-motion dependency and implement animated text cycle component; update Home page layout and add profile picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,31 @@
+import ClipPathImage from "@/components/element/clip-path-image";
+import AnimatedTextCycle from "@/components/ui/animated-text-cycle";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const words = [
+    "live on the internet.",
+    "solve user problems.",
+    "are fast and responsive.",
+    "look great on any device.",
+    "are accessible to everyone.",
+    "push the boundaries of the web.",
+  ];
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center min-h-screen px-4">
+      <main className="grid grid-cols-1 md:grid-cols-2 items-center sm:items-start gap-8">
+        <div className="relative">
+          <h1 className="text-2xl font-medium text-center sm:text-left">
+            👋 Hello, I’m Mantha
+          </h1>
+          <h1 className="mt-4 text-3xl text-muted-foreground font-semibold text-center sm:text-left">
+            I’m a software engineer and designer based in Cambodia. I love to
+            create things that <span><AnimatedTextCycle words={words} className="font-semibold text-foreground"/></span>
+          </h1>
+        </div>
+        <div className="w-60 md:w-90 relative">
+          <ClipPathImage />
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
@@ -65,7 +42,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Home
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -80,7 +57,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Projects
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -95,7 +72,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          About →
         </a>
       </footer>
     </div>
